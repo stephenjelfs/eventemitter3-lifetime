22b0a572cd6610770f8197593c08faf4d79b3557
stephenjelfs <stephenjelfs@Carolines-iMac-4.local>
2016-05-01 15:16:26 +0200

    Combined event emitter utils with lifetime.

--- src/Lifetime.spec.ts
+++ src/Lifetime.spec.ts
@@ -1,12 +1,12 @@
 import {Lifetime} from "../src/Lifetime";
-import {LifetimeImpl} from "../src/Lifetime";
+import * as EventEmitter from "eventemitter3";
 import createSpy = jasmine.createSpy;
 import Spy = jasmine.Spy;
 
 describe("Lifetime", function() {
 
     it("should notify end of life listeners", function() {
-        let life: LifetimeImpl = new LifetimeImpl();
+        let life: Lifetime = new Lifetime();
 
         let listener1: Spy = createSpy("listener1");
         let listener2: Spy = createSpy("listener2");
@@ -24,11 +24,46 @@ describe("Lifetime", function() {
     });
 
     it("should not allow new listeners after life is ended", function() {
-        let life: LifetimeImpl = new LifetimeImpl();
+        let life: Lifetime = new Lifetime();
 
         expect(() => life.onEndOfLife(() => {}, this)).not.toThrow();
         life.endLife();
         expect(() => life.onEndOfLife(() => {}, this)).toThrow();
+    });
+
+    it("should end when parent life end", function() {
+        let parentLife: Lifetime = new Lifetime();
+        let life: Lifetime = new Lifetime(parentLife);
+
+        expect(() => life.onEndOfLife(() => {}, this)).not.toThrow();
+        parentLife.endLife();
+        expect(() => life.onEndOfLife(() => {}, this)).toThrow();
+    });
+
+    it("should remove event emitter listeners on end of life", function() {
+        let life: Lifetime = new Lifetime();
+        let eventEmitter: EventEmitter3.EventEmitter = new EventEmitter();
+
+        let listener1: Spy = createSpy("listener1");
+        let listener2: Spy = createSpy("listener2");
+
+        life.on(eventEmitter, "change", listener1, this);
+        life.on(eventEmitter, "change", listener2, this);
+        expect(eventEmitter.listeners("change").length).toBe(2);
+
+        expect(listener1).not.toHaveBeenCalled();
+        expect(listener2).not.toHaveBeenCalled();
+
+        eventEmitter.emit("change");
+        expect(listener1).toHaveBeenCalledTimes(1);
+        expect(listener2).toHaveBeenCalledTimes(1);
+
+        life.endLife();
+        expect(eventEmitter.listeners("change").length).toBe(0);
+
+        eventEmitter.emit("change");
+        expect(listener1).toHaveBeenCalledTimes(1);
+        expect(listener2).toHaveBeenCalledTimes(1);
     });
 
 });
--- src/Lifetime.ts
+++ src/Lifetime.ts
@@ -2,24 +2,60 @@ import * as EventEmitter from "eventemitter3";
 
 const END_OF_LIFE: string = "endOfLife";
 
-export interface Lifetime {
-    onEndOfLife(listener: Function, context: any): void;
+export interface EventEmitterLike {
+  on(event: string, fn: Function, life: Lifetime, context?: any): EventEmitterLike;
+  off(event: string, fn?: Function, context?: any, once?: boolean): EventEmitterLike;
 }
 
-export class LifetimeImpl implements Lifetime {
+export class Lifetime {
 
     private eventEmitter: EventEmitter3.EventEmitter = new EventEmitter();
 
-    onEndOfLife(listener: () => void, context: any): void {
+    constructor(parentLife?: Lifetime) {
+      if (parentLife) {
+        parentLife.onEndOfLife(() => this.endLife(), this);
+      }
+    }
+
+    /**
+     * Register an end of life listener.
+     *
+     * @param {Function} fn Callback function.
+     * @param {Mixed} [context=this] The context of the function.
+     * @api public
+     */
+    onEndOfLife(fn: Function, context: any): void {
         if (this.eventEmitter == null) {
             throw "Life already ended.";
         }
-        this.eventEmitter.addListener(END_OF_LIFE, listener, context);
+        this.eventEmitter.addListener(END_OF_LIFE, fn, context);
     }
 
+
+    /**
+     * Fires an end of life event.
+     *
+     * @api public
+     */
     endLife(): void {
         this.eventEmitter.emit(END_OF_LIFE);
         this.eventEmitter.removeAllListeners(END_OF_LIFE);
         this.eventEmitter = null;
+    }
+
+    /**
+     * Register a new EventListener with an {EventEmitter} for the given event,
+     * until this life is ended.
+     *
+     * @param {EventEmitter} eventEmitter Name of the event.
+     * @param {String} event Name of the event.
+     * @param {Function} fn Callback function.
+     * @param {Mixed} [context=this] The context of the function.
+     * @api public
+     */
+    on(eventEmitter: EventEmitterLike, event: string, fn: Function, context?: any): Lifetime {
+      eventEmitter.on(event, fn, context);
+      this.onEndOfLife(() => eventEmitter.off(event, fn, context), context);
+      return this;
     }
 }
